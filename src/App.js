b7504b33ea9b8d55e1854a12ad1ae4e58813301b
Juan Patricio Lusardi <jlusardi@Juans-MacBook-Pro.local>
2018-09-04 21:10:41 -0300

    Change <p> tag to <div>  to fix error in Console

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to Coderhouse</h1>
         </header>
-        <p className="App-intro">
+        <div className="App-intro">
           <ItemCard title={"Hola"} description={"Chau"} />
-        </p>
+        </div>
       </div>
     );
   }
